5fde1c004bd12aba12be8eccb48f17b5cdae64cc
matthewhinrichsen96 <matthewhinrichsen@gmail.com>
2025-09-28 16:57:51 +1000

    Test items

---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// using MessengerApp.Services.Models.Messages;
+// using MessengerApp.Services.Services;
+//
+// var redisService = new MessengerService();
+// var input = Console.ReadLine() ?? "This was null";
+//
+// var message = new Message()
+// {
+//     chatId = "456789",
+//     senderId = "12342",
+//     text = input,
+//     timeStamp =  DateTime.UtcNow.ToString("o")
+// };
+//
+// redisService.SendMessage(message);
+// await redisService.GetMessages("12342", "456789");
+//
+ Console.ReadLine();
+
